ce3b256d7cebac71a1b2978cee56315283cbfea1
HEXER22 <40181915+HEXER22@users.noreply.github.com>
2018-06-14 13:31:40 +0530

    Add files via upload

---
+++ Lazymux.sh
@@ -0,0 +1,16 @@
+apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
+
+apt-get install git
+
+apt-get install python2 
+
+git clone https://github.com/Gameye98/Lazymux
+
+cd Lazymux
+
+ls 
+
+python2 Lazymux.py
+
+
+
